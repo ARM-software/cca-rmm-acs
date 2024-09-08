9461938168c493d91e3bc3951366dae1c93506fc
Shyamanth RH <shyamanth.rh@arm.com>
2024-09-08 15:02:38 +0000

    fix : Exception - Use Arch timer to calculate boot time and then trigger interrupt
    Signed-off-by: Shyamanth RH <shyamanth.rh@arm.com>

--- test/exception/exception_rec_exit_irq/exception_rec_exit_irq_host.c
+++ test/exception/exception_rec_exit_irq/exception_rec_exit_irq_host.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Arm Limited or its affiliates. All rights reserved.
+ * Copyright (c) 2023-2024, Arm Limited or its affiliates. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -7,6 +7,7 @@
 #include "test_database.h"
 #include "val_host_rmi.h"
 #include "val_irq.h"
+#include "val_timer.h"
 
 static volatile int handler_flag;
 
@@ -23,6 +24,8 @@ void exception_rec_exit_irq_host(void)
     val_host_realm_ts realm;
     uint64_t ret;
     val_host_rec_exit_ts *rec_exit = NULL;
+    val_host_rec_enter_ts *rec_enter = NULL;
+    uint64_t cnt_val1, cnt_val2, cnt_frq, boot_time_ms;
 
     val_host_realm_params(&realm);
 
@@ -34,32 +37,63 @@ void exception_rec_exit_irq_host(void)
         goto destroy_realm;
     }
 
+    rec_enter = &(((val_host_rec_run_ts *)realm.run[0])->enter);
     rec_exit = &(((val_host_rec_run_ts *)realm.run[0])->exit);
 
-    if (val_irq_register_handler(PLATFORM_NS_WD_INTR, wd_irq_handler))
-    {
-        LOG(ERROR, "\tWD interrupt register failed\n", 0, 0);
-        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(2)));
-        goto destroy_realm;
-    }
-
-    val_irq_enable(PLATFORM_NS_WD_INTR, 0);
-
-    val_ns_wdog_enable(100);
+    /* Read Counter before entering realm */
+    cnt_val1 = val_read_cntpct_el0();
 
     ret = val_host_rmi_rec_enter(realm.rec[0], realm.run[0]);
     if (ret)
     {
         LOG(ERROR, "\tRec enter failed, ret=%x\n", ret, 0);
+        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(2)));
+        goto destroy_realm;
+    }
+
+    /* Check that REC exit was due to host call after realm is booted */
+    if (rec_exit->exit_reason != RMI_EXIT_HOST_CALL) {
+        LOG(ERROR, "\tUnexpected REC exit, %d. ESR: %lx \n", rec_exit->exit_reason, rec_exit->esr);
         val_set_status(RESULT_FAIL(VAL_ERROR_POINT(3)));
+        goto destroy_realm;
+    }
+
+    /* Read counter after coming back to host */
+    cnt_val2 = val_read_cntpct_el0();
+
+    /* Register and enable IRQ handler */
+    if (val_irq_register_handler(PLATFORM_NS_WD_INTR, wd_irq_handler))
+    {
+        LOG(ERROR, "\tWD interrupt register failed\n", 0, 0);
+        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(4)));
+        goto destroy_realm;
+    }
+
+    val_irq_enable(PLATFORM_NS_WD_INTR, 0);
+
+    /* Calculate the boot time of the platform */
+    cnt_frq = val_read_cntfrq_el0();
+    boot_time_ms = ((cnt_val2 - cnt_val1) * 1000) / cnt_frq;
+
+    /* Enable Watchdog timer and enter Realm */
+    val_ns_wdog_enable((uint32_t)(boot_time_ms * 100000));
+
+    rec_enter->gprs[1] = boot_time_ms;
+
+    ret = val_host_rmi_rec_enter(realm.rec[0], realm.run[0]);
+    if (ret)
+    {
+        LOG(ERROR, "\tRec enter failed, ret=%x\n", ret, 0);
+        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(5)));
         goto free_irq;
     }
 
+    /* Check REC exit is due to IRQ */
     if ((rec_exit->exit_reason != RMI_EXIT_IRQ) || rec_exit->esr)
     {
         LOG(ERROR, "\tRec exit params mismatch, exit_reason=%x esr %lx\n",
                         rec_exit->exit_reason, rec_exit->esr);
-        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(4)));
+        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(6)));
         goto free_irq;
     }
 
@@ -68,7 +102,7 @@ void exception_rec_exit_irq_host(void)
         LOG(ALWAYS, "\tWD Interrupt triggered\n", 0, 0);
     } else {
         LOG(ERROR, "\tWD interrupt not triggered\n", 0, 0);
-        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(5)));
+        val_set_status(RESULT_FAIL(VAL_ERROR_POINT(7)));
         goto free_irq;
     }
 
